41a9a7a9a10b4f6ae8af58f27df707a8871687c6
Aldair Huamani Cusi <32407895+Baku452@users.noreply.github.com>
2024-10-02 01:11:39 -0500

    Add(-): fix turso cli

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -4,7 +4,11 @@ export const typeOrmConfig = (
     configService: ConfigService,
 ): TypeOrmModuleOptions => ({
     type: 'sqlite',
-    database: configService.get<string>('DATABASE_URL'),
+    driver: require('@libsql/sqlite3'),
+    flags: 0x00000040,
+    database: `${configService.get<string>(
+        'TURSO_DATABASE_URL',
+    )}?authToken=${configService.get<string>('TURSO_AUTH_TOKEN')}`,
     autoLoadEntities: true,
     synchronize: configService.get<string>('NODE_ENV') !== 'production',
     logging: configService.get<string>('NODE_ENV') === 'development',
